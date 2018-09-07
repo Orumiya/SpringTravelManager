efeab1cdb050566ee38f7cdd82fbb450242151b4
Orumiya <kasza.eszter.katalin@gmail.com>
2018-09-07 23:38:41 +0200

    Travel entity added

---
+++ src/main/java/com/triptech/entities/Travel.java
@@ -0,0 +1,111 @@
+package com.triptech.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Travel {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String travelName;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String transport;
+    private Integer minNumber;
+    private Integer maxNumber;
+    private Integer childPrice;
+    private Integer adultPrice;
+
+    @ManyToMany
+    private List<Place> placeList;
+
+    @ManyToMany
+    private List<TourProgram> programList;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTravelName() {
+        return travelName;
+    }
+
+    public void setTravelName(String travelName) {
+        this.travelName = travelName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTransport() {
+        return transport;
+    }
+
+    public void setTransport(String transport) {
+        this.transport = transport;
+    }
+
+    public Integer getMinNumber() {
+        return minNumber;
+    }
+
+    public void setMinNumber(Integer minNumber) {
+        this.minNumber = minNumber;
+    }
+
+    public Integer getMaxNumber() {
+        return maxNumber;
+    }
+
+    public void setMaxNumber(Integer maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    public Integer getChildPrice() {
+        return childPrice;
+    }
+
+    public void setChildPrice(Integer childPrice) {
+        this.childPrice = childPrice;
+    }
+
+    public Integer getAdultPrice() {
+        return adultPrice;
+    }
+
+    public void setAdultPrice(Integer adultPrice) {
+        this.adultPrice = adultPrice;
+    }
+}
