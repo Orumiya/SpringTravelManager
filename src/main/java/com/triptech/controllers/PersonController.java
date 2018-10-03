9471be06fb8689211ce297757889c9de733f16c4
Orumiya <kasza.eszter.katalin@gmail.com>
2018-10-03 10:23:14 +0200

    travelguide page added

--- src/main/java/com/triptech/controllers/PersonController.java
+++ src/main/java/com/triptech/controllers/PersonController.java
@@ -2,8 +2,10 @@ package com.triptech.controllers;
 
 import com.triptech.entities.Customer;
 import com.triptech.entities.Person;
+import com.triptech.entities.TravelGuide;
 import com.triptech.repositories.CustomerRepository;
 import com.triptech.repositories.PersonRepository;
+import com.triptech.repositories.TravelGuideRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,11 +26,16 @@ public class PersonController {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    TravelGuideRepository travelGuideRepository;
+
     @GetMapping("FirstName")
     @ResponseBody  //ha vissza akarok térni stringgel vagy JSON-osítva
     public String getFirstName(){
        return personRepository.findAll().get(0).getFirstName();
     }
+
+    //******************************** Customer Controller starts **********************************************************
 
     @GetMapping("customerList") //visszatérési érték alapján egy view resolvert keres (template nevet)
     public String getCustomerList(Model model){
@@ -66,11 +73,47 @@ public class PersonController {
         }
     }
 
+    //******************************** Customer Controller ends **********************************************************
     @ModelAttribute
     public void addAttributes(Model model) {
         model.addAttribute("msg", "Welcome!");
     }
 
+    //********************************************Travelguide Controller ********************************************
+    @GetMapping("travelguideList")
+    public String getTravelguideList(Model model){
+        model.addAttribute("travelguides",travelGuideRepository.findAll());
+        model.addAttribute("travelguide", new TravelGuide());
+        return "travelguide";
+    }
 
+    private Map<Long, TravelGuide> travelguideMap = new HashMap<>();
+
+    @RequestMapping(value = "/saveTravelguide", method = RequestMethod.POST)
+    public String submit(
+            @ModelAttribute("travelguide") TravelGuide travelguide,
+            BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }else {
+            model.addAttribute("firstName", travelguide.getFirstName());
+            model.addAttribute("lastName", travelguide.getLastName());
+            model.addAttribute("birthDate", travelguide.getBirthDate());
+            model.addAttribute("idType",travelguide.getIdType());
+            model.addAttribute("idNumber",travelguide.getIdNumber());
+            model.addAttribute("addressCountry",travelguide.getAddressCountry());
+            model.addAttribute("addressCity",travelguide.getAddressCity());
+            model.addAttribute("addressFree",travelguide.getAddressFree());
+            model.addAttribute("dailyAllowance", travelguide.getDailyAllowance());
+            model.addAttribute("taxId", travelguide.getTaxId());
+
+
+            travelguideMap.put(travelguide.getId(), travelguide);
+            travelGuideRepository.save(travelguide);
+            model.addAttribute("travelguides", travelGuideRepository.findAll());
+            model.addAttribute("travelguide", new TravelGuide());
+            return "travelguide";
+        }
+    }
 
 }
